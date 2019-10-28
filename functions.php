92b4b110473898d3e19f5916583903ac2f6bc634
knjenga85 <53800393+knjenga85@users.noreply.github.com>
2019-10-28 12:34:23 -0400

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,55 @@
+<?php // PHP - Random Quote Generator
+
+// Create the Multidimensional array of quote elements and name it quotes
+// Each inner array element should be an associative array
+$quotes = array(
+
+
+["quote" => "Change the world by being yourself"]
+["source" => "Amy Poehler"]
+,
+["quote" => "Every moment is a fresh beginning"]
+["source" => "TS Eliot"]
+,
+["quote" => "Never regret anything that made you smile"]
+["source" => "Mark Twain"]
+,
+["quote" => "Everything you can imagine is real"]
+["source" => "Pablo Picasso"]
+,
+["quote" => "Simplicity is the ultimate sophistication"]
+["source" => "Leonardo da Vinci"]
+,
+["quote" => "Tough times never last but tough people do"]
+["source" => "Robert H Schiuller"]
+
+
+);
+
+
+
+// Create the getRandomQuuote function and name it getRandomQuote
+
+  function getRandomQuote($quotes){
+
+  return $quotes[rand(0, 5)]; // Calls random number
+
+
+
+}
+
+
+// Create the printQuote funtion and name it printQuote
+Function printquote() {
+
+  $randomQuote = getRandomQuote($quotes);
+
+ $quote = getRandomQuote($quotes); 
+   $str = '';
+   $string .= "<p class='quote'>" . $quote["quote"] . "</p>";
+   $string .= "<p class='source'>" . $quote["source"];
+   echo $string
+}
+
+
+?>
